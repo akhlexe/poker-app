34aa64a1c8ad603337c798e627eb1588c0552672
Exequiel <exe.pine@gmail.com>
2026-01-17 09:19:39 -0300

    feat, improving cards

--- poker-client/src/components/CardView.tsx
+++ poker-client/src/components/CardView.tsx
@@ -1,14 +1,34 @@
 import type { Card } from "../models/Card";
+import styles from "./CardView.module.css";
 
 interface Props {
   card: Card;
 }
 
+const getSuitClass = (suit: string) => {
+  switch (suit) {
+    case "♠":
+      return styles.spades;
+    case "♥":
+      return styles.hearts;
+    case "♦":
+      return styles.diamonds;
+    case "♣":
+      return styles.clubs;
+    default:
+      return "";
+  }
+};
+
 export function CardView({ card }: Props) {
   return (
-    <span className="card">
-      {card.rank}
-      {card.suit}
-    </span>
+    <div className={styles.card}>
+      <span className={`${styles.rank} ${getSuitClass(card.suit)}`}>
+        {card.rank}
+      </span>
+      <span className={`${styles.suit} ${getSuitClass(card.suit)}`}>
+        {card.suit}
+      </span>
+    </div>
   );
 }
